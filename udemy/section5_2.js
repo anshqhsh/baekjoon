79fd1322e0fbd33f5c7b3dd746f6c138eb83eda6
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-03-27 12:37:11 +0900

    section5

---
+++ udemy/section5_2.js
@@ -0,0 +1,26 @@
+function validAnagram(char1, char2) {
+  if (char1.length !== char2.length) {
+    return false;
+  }
+  const lookup = {};
+
+  for (let i = 0; i < char1.length; i++) {
+    let letter = char1[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < char2.length; i++) {
+    let letter = char2[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+console.log(validAnagram('', '')); // true
+console.log(validAnagram('aaz', 'zza')); // f
+console.log(validAnagram('anagram', 'nagaram')); // true
+console.log(validAnagram('rat', 'car')); // f
+console.log(validAnagram('qwerty', 'qeywet')); // f
